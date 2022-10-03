d3c0d0d67170aae3295d11de7a46efd70f9a3462
SpeedyAPM <97914085+SpeedyAPM@users.noreply.github.com>
2022-10-03 20:17:48 +0000

    Digits after clicking go white

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ s.listen()
 
 
 
-def box_draw(pos_x: int, pos_y: int, size: int):
+def box_draw(pos_x: int, pos_y: int, size: int, digit: int):
     '''Making a box a 'size' value away in all directions starting from (pos_x, pos_y)'''
     t.penup()
     t.goto(pos_x + size,pos_y)
@@ -32,15 +32,20 @@ def box_draw(pos_x: int, pos_y: int, size: int):
     t.penup()
     t.goto(pos_x,pos_y)
     t.pendown()
+    t.penup()
+    t.goto(pos_x,pos_y - 18)
+    t.write(str(digit),True, align='center',font=('Arial',18,'normal'))
+    t.penup()
 
 class meshxy:
     xpoint = int
     ypoint = int
     visibp = bool
     clicked = bool
+    digit = int
 
-wymiar1 = 12
-wymiar2 = 12
+wymiar1 = 10
+wymiar2 = 10
 superlista = []
 
 for ii in range (0,wymiar1):
@@ -53,17 +58,20 @@ for ii in range (0,wymiar1):
 # for ii in range (0,10):
 #     print(superlista[ii])
 
-xx1 = -100
-yy1 = -100
-rozmiar = 8
+xx1 = -270
+yy1 = -200
+rozmiar = 15
+skok = 38
+how_many_digits = 10
 
 for ii in range (0,wymiar1):
-    yy1 = yy1 + 20
+    yy1 = yy1 + skok
 
     for jj in range(0, wymiar2):
         superlista[ii][jj].xpoint = xx1
         superlista[ii][jj].ypoint = yy1
-        xx1 = xx1 + 20
+        xx1 = xx1 + skok
+        superlista[ii][jj].digit = randint(1,how_many_digits + 1)
         superlista[ii][jj].clicked = False
         superlista[ii][jj].visibp = False
         if (randint(1,3)%3) == 0:
@@ -77,7 +85,7 @@ for ii in range (0,wymiar1):
     for jj in range(0, wymiar2):
         #print(superlista[ii][jj].xpoint , 'Y:' , superlista[ii][jj].ypoint)
         if superlista[ii][jj].visibp == True:
-          box_draw(superlista[ii][jj].xpoint, superlista[ii][jj].ypoint , rozmiar)
+          box_draw(superlista[ii][jj].xpoint, superlista[ii][jj].ypoint , rozmiar, superlista[ii][jj].digit)
 
 
 def fpoint(x, y):
@@ -90,7 +98,7 @@ def fpoint(x, y):
         if ((x < (xs1 + rozmiar)) and (x > (xs1 - rozmiar) ) and (y < (ys1 + rozmiar)) and (y > (ys1 - rozmiar) )):
             superlista[ii][jj].clicked = True
             t.color('white')
-            box_draw(superlista[ii][jj].xpoint, superlista[ii][jj].ypoint , rozmiar)
+            box_draw(superlista[ii][jj].xpoint, superlista[ii][jj].ypoint , rozmiar, superlista[ii][jj].digit)
             t.color('black')
 
 
