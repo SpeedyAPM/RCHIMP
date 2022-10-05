70fa2db0e4c7a8292a0196c9d09b3a767b6c698d
SpeedyAPM <97914085+SpeedyAPM@users.noreply.github.com>
2022-10-05 21:14:49 +0000

    Engine finished

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ Check out the official docs here: https://docs.python.org/3/library/turtle.html
 from random import randint
 import turtle
 
+
+
+
 t = turtle.Turtle()
 s = turtle.Screen()
 
@@ -24,8 +27,6 @@ def box_draw(pos_x: int, pos_y: int, size: int, digit: int):
     t.goto(pos_x + size,pos_y)
     t.penup()
     t.goto(pos_x,pos_y)
-    t.pendown()
-    t.penup()
     t.goto(pos_x,pos_y - 18)
     t.write(str(digit),True, align='center',font=('Arial',18,'normal'))
     t.penup()
@@ -45,10 +46,27 @@ def box_erase(pos_x: int, pos_y: int, size: int, digit: int):
     t.end_fill()
     t.penup()
     t.goto(pos_x,pos_y)
-    t.pendown()
-    t.penup()
     t.goto(pos_x,pos_y - 18)
     t.write(str(digit),True, align='center',font=('Arial',18,'normal'))
+    t.penup()
+
+def box_hide(pos_x: int, pos_y: int, size: int, digit: int):
+    '''Making a box a 'size' value away in all directions starting from (pos_x, pos_y)'''
+    t.penup()
+    t.goto(pos_x + size,pos_y)
+    t.pendown()
+    t.fillcolor ("black")
+    t.begin_fill()
+    t.goto(pos_x + size,pos_y + size) # 1
+    t.goto(pos_x - size,pos_y + size) # 2
+    t.goto(pos_x - size,pos_y - size) # 3
+    t.goto(pos_x + size,pos_y - size) # 4
+    t.goto(pos_x + size,pos_y)
+    t.end_fill()
+    t.penup()
+    t.goto(pos_x,pos_y)
+    t.goto(pos_x,pos_y - 18)
+    #t.write(str(digit),True, align='center',font=('Arial',18,'normal'))
     t.penup()
 
 class meshxy:
@@ -76,7 +94,8 @@ xx1 = -270
 yy1 = -200
 rozmiar = 15
 skok = 38
-how_many_digits = 10
+how_many_digits = 8
+number_tc = 1
 
 for ii in range (0,wymiar1):
     yy1 = yy1 + skok
@@ -85,13 +104,47 @@ for ii in range (0,wymiar1):
         superlista[ii][jj].xpoint = xx1
         superlista[ii][jj].ypoint = yy1
         xx1 = xx1 + skok
-        superlista[ii][jj].digit = randint(1,how_many_digits + 1)
+        #superlista[ii][jj].digit = randint(1,how_many_digits + 1)
         superlista[ii][jj].clicked = False
         superlista[ii][jj].visibp = False
-        if (randint(1,3)%3) == 0:
-          superlista[ii][jj].visibp = True
+        #if (randint(1,3)%3) == 0:
+         # superlista[ii][jj].visibp = True
         #print(superlista[ii][jj].xpoint , 'Y:' , superlista[ii][jj].ypoint)
     xx1 = -100
+
+
+
+def przeszukiwanie(x,y):
+  global number_tc
+  for ii in range (0,wymiar1):    
+    for jj in range(0, wymiar2):
+      xs1 =superlista[ii][jj].xpoint
+      ys1 =superlista[ii][jj].ypoint
+      if ((x < (xs1 + rozmiar)) and (x > (xs1 - rozmiar) ) and (y < (ys1 + rozmiar)) and (y > (ys1 - rozmiar) )):
+        if (superlista[ii][jj].digit == number_tc and superlista[ii][jj].clicked == False):
+          number_tc += 1
+          
+          superlista[ii][jj].clicked = True
+          superlista[ii][jj].visibp == False
+          t.color('white')
+          box_erase(superlista[ii][jj].xpoint, superlista[ii][jj].ypoint , rozmiar, superlista[ii][jj].digit)
+          przerysowanie()
+          t.color('black')
+
+    
+
+
+
+# Losowanie cyfr
+for digitsall in range (1,how_many_digits):
+  while True:
+    ii = randint(0, wymiar1 -1)
+    jj = randint(0, wymiar2 -1)
+    if (superlista[ii][jj].visibp ==False):
+      superlista[ii][jj].visibp = True
+      superlista[ii][jj].digit = digitsall
+    if superlista[ii][jj].visibp == True:
+        break
 
 # Rysowanie
 for ii in range (0,wymiar1):
@@ -100,18 +153,18 @@ for ii in range (0,wymiar1):
         if superlista[ii][jj].visibp == True:
           box_draw(superlista[ii][jj].xpoint, superlista[ii][jj].ypoint , rozmiar, superlista[ii][jj].digit)
 
+def przerysowanie():
+  for ii in range (0,wymiar1):
+    for jj in range(0, wymiar2):
+        #print(superlista[ii][jj].xpoint , 'Y:' , superlista[ii][jj].ypoint)
+        if superlista[ii][jj].visibp == True and superlista[ii][jj].clicked == False:
+          box_hide(superlista[ii][jj].xpoint, superlista[ii][jj].ypoint , rozmiar, superlista[ii][jj].digit)
+
 def fpoint(x, y):
     #t.goto(x, y)
     #t.write(str(x) + "," + str(y))
-    for ii in range (0,wymiar1):    
-      for jj in range(0, wymiar2):
-        xs1 =superlista[ii][jj].xpoint
-        ys1 =superlista[ii][jj].ypoint
-        if ((x < (xs1 + rozmiar)) and (x > (xs1 - rozmiar) ) and (y < (ys1 + rozmiar)) and (y > (ys1 - rozmiar) )):
-            superlista[ii][jj].clicked = True
-            t.color('white')
-            box_erase(superlista[ii][jj].xpoint, superlista[ii][jj].ypoint , rozmiar, superlista[ii][jj].digit)
-            t.color('black')
+  przeszukiwanie(x,y)
+
           
 s.onclick(fpoint, "1") # lewy przycisk myszki
 s.onclick(fpoint, "3") # prawy przycisk myszki
